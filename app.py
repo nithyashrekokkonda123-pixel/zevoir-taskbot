84cc0d153e38d57a52ae9612596cb5f6dd547a61
nithyashre-k <nithyashrekokkonda123@gmail.com>
2026-03-06 12:02:24 +0530

    modified app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.secret_key = "zevoir-secret-key-2024"   # required to use Flask sessions
 
 # ── The public API we read todos from ────────────────────────────────────────
-TODOS_API = "https://jsonplaceholder.typicode.com/todos"
+TODOS_API = "https://jsonplaceholder.typicode.com/todosss"
 
 
 # ─────────────────────────────────────────────────────────────────────────────
